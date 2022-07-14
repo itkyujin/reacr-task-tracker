076a3487d78320414d89ec5eb3b3c35330aae0ae
itkyujin <itkyujin@gmail.com>
2022-07-14 19:03:56 -0400

    Header_AfterPrototype

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+
+const Header = ({ title }) => {
   return (
     <header>
-        <h1>Task Tracker</h1>
+        <h1>{title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.prototype = {
+    title: PropTypes.string,
 }
 
 export default Header
