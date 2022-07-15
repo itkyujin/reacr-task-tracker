dbf9c5bed6f196fc178c99eecec7c2450044b2be
itkyujin <itkyujin@gmail.com>
2022-07-15 07:21:22 -0400

    App_AddRouterAfter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import Header from "./components/Header"
 
 function App() { 
+
+
+
   return (
-    <div className="container">
-       <Header />   
-    </div>
+    <Router>
+      <div className='container'>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Routes>
+          <Routes
+            path='/'
+            element={
+              <>
+                {showAddTask && <AppTask onAdd={addTak} />}
+                {tasks.lengh > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />  
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            }
+          />
+        <Routes path='/about' element={<About />} />
+      </Routes>
+      <Footer />
+      </div>
+    </Router>
   )
 }
 
